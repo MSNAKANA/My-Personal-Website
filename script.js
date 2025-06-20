a5857ac3b27ad4cc77f3fc951c11737936a90a5b
Mohlala Shaun Nakana <134694005+MSNAKANA@users.noreply.github.com>
2025-06-20 20:56:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Update URL hash
 // window.location.hash = sectionId;
 
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
 function showSection(sectionId) {
     document.querySelectorAll('.section').forEach(section => {
         section.classList.remove('active');
